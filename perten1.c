5665368483920a79444eb2a40a3ee5554dd7fde5
Bhumil Mayani <bhumilmayani12@gmail.com>
2025-11-22 17:40:23 +0530

    this is a perten program using c language

---
+++ perten1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    int i,j,x,n,a,b,c;
+    printf("enter the number");
+    scanf("%d",&n);
+        for(i=1;i<=n;i++)
+     {
+        for(j=0;j<n-i;j++)
+        {
+          printf(" ");
+        }
+        for(j=n-i;j<n;j+i)
+        { 
+            for(x=1,a=1,b=1,c=0;x<=i;x++)
+            {
+              a+=c;
+              printf("%d",a);
+              c=b;
+              b=a;
+            }
+        }
+        printf("\n");
+    }
+}
